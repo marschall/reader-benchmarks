8ed4fc2f5a537480d4e6138e4e529d5a98889d47
Philippe Marschall <philippe.marschall@gmail.com>
2020-12-24 17:18:35 +0100

    run script

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export JAVA_HOME=${HOME}/tmp/JDK-4926314/unpatched/jdk
+$JAVA_HOME/bin/java -cp target/benchmarks.jar  -Djmh.blackhole.mode=COMPILER com.github.marschall.readerbenchmarks.Main 4926314-before
+
+export JAVA_HOME=${HOME}/tmp/JDK-4926314/patched/jdk
+$JAVA_HOME/bin/java -cp target/benchmarks.jar  -Djmh.blackhole.mode=COMPILER com.github.marschall.readerbenchmarks.Main 4926314-after
+
