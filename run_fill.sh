cd62fae213d276ec741e4afda1436291d9f3defc
Philippe Marschall <philippe.marschall@gmail.com>
2020-12-27 15:23:33 +0100

    jpms

--- run_fill.sh
+++ run_fill.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 export JAVA_HOME=${HOME}/tmp/JDK-4926314/patched/jdk
-$JAVA_HOME/bin/java -cp target/benchmarks.jar  -Djmh.blackhole.mode=COMPILER com.github.marschall.readerbenchmarks.ScannerTests
+
+$JAVA_HOME/bin/java -cp target/benchmarks.jar --add-exports java.management/sun.management=ALL-UNNAMED --add-opens java.management/sun.management=ALL-UNNAMED -Djmh.blackhole.mode=COMPILER com.github.marschall.readerbenchmarks.ScannerTests
 
