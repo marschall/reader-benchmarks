f12af1f3a469c4d97eff9cf0847e5a5edf079f3d
Philippe Marschall <philippe.marschall@gmail.com>
2020-12-28 22:28:07 +0100

    replace existing files

--- src/main/java/com/github/marschall/readerbenchmarks/Main.java
+++ src/main/java/com/github/marschall/readerbenchmarks/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
@@ -30,7 +31,7 @@ public class Main {
 //    Path.of("jmh-result.csv");
     Path jmhResult = Paths.get("jmh-result.csv");
     if (Files.exists(jmhResult) && (args.length > 0)) {
-      Files.move(jmhResult, Paths.get(args[0] + "-jmh-result.csv"));
+      Files.move(jmhResult, Paths.get(args[0] + "-jmh-result.csv"), StandardCopyOption.REPLACE_EXISTING);
     }
   }
 
