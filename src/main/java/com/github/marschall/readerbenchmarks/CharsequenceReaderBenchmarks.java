2212dc7401219813d646bd2c5f249382fc99a7ff
Philippe Marschall <philippe.marschall@gmail.com>
2021-01-31 14:57:06 +0100

    add benchmarks for more Readers

---
+++ src/main/java/com/github/marschall/readerbenchmarks/CharsequenceReaderBenchmarks.java
@@ -0,0 +1,87 @@
+package com.github.marschall.readerbenchmarks;
+
+import static java.util.concurrent.TimeUnit.MICROSECONDS;
+import static org.openjdk.jmh.annotations.Mode.Throughput;
+import static org.openjdk.jmh.annotations.Scope.Benchmark;
+
+import java.io.CharArrayReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.util.Arrays;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+@BenchmarkMode(Throughput)
+@OutputTimeUnit(MICROSECONDS)
+@State(Benchmark)
+public class CharsequenceReaderBenchmarks {
+
+  private CharBuffer charBuffer;
+
+  @Param({"true", "false"})
+  boolean onHeap;
+
+  @Param({"true", "false"})
+  boolean latin1;
+
+  @Param({"128", "1024"})
+  int transferSize;
+
+  private Reader stringReader;
+
+  private Reader charArrayReader;
+
+  @Setup
+  public void setup() throws IOException {
+    if (this.onHeap) {
+      this.charBuffer = CharBuffer.allocate(this.transferSize);
+    } else {
+      this.charBuffer = ByteBuffer.allocateDirect(this.transferSize).asCharBuffer();
+    }
+    char[] c;
+    if (this.latin1) {
+      c = this.createLatin1Array(this.transferSize);
+    } else {
+      c = this.createNonLatin1Array(this.transferSize);
+    }
+    this.stringReader = new StringReader(new String(c));
+    this.stringReader.mark(this.transferSize);
+    this.charArrayReader = new CharArrayReader(c);
+    this.charArrayReader.mark(this.transferSize);
+  }
+
+  private char[] createLatin1Array(int size) {
+    char[] c = new char[size];
+    Arrays.fill(c, 'A');
+    return c;
+  }
+
+  private char[] createNonLatin1Array(int size) {
+    char[] c = new char[size];
+    Arrays.fill(c, '\u20AC');
+    return c;
+  }
+
+  @Benchmark
+  public int readStringReader() throws IOException {
+    this.charBuffer.clear();
+    this.stringReader.reset();
+    return this.stringReader.read(this.charBuffer);
+  }
+
+  @Benchmark
+  public int readCharArrayReader() throws IOException {
+    this.charBuffer.clear();
+    this.charArrayReader.reset();
+    return this.charArrayReader.read(this.charBuffer);
+  }
+
+}
