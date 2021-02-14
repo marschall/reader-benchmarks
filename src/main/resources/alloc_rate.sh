444056139076efd22964bb75c214d80450149c72
Philippe Marschall <philippe.marschall@gmail.com>
2021-02-14 15:09:47 +0100

    udpated excel sheets

--- src/main/resources/alloc_rate.sh
+++ src/main/resources/alloc_rate.sh
@@ -2,6 +2,4 @@
 
 grep gc.alloc.rate 4926314-before-jmh-result.csv           | grep -v norm > 4926314-off-before-jmh-result-alloc-rate.csv
 grep gc.alloc.rate 4926314-after-jmh-result.csv            | grep -v norm > 4926314-after-jmh-result-alloc-rate.csv
-grep gc.alloc.rate 4926314-no-streamdecoder-jmh-result.csv | grep -v norm > 4926314-no-streamdecoder-jmh-result-alloc-rate.csv
-grep gc.alloc.rate 4926314-off-on-heap-jmh-result.csv      | grep -v norm > 4926314-off-on-heap-jmh-result-alloc-rate.csv
 
--- src/main/resources/prune.sh
+++ src/main/resources/prune.sh
@@ -2,6 +2,4 @@
 
 grep -v gc 4926314-before-jmh-result.csv           > 4926314-off-before-jmh-result-pruned.csv
 grep -v gc 4926314-after-jmh-result.csv            > 4926314-after-jmh-result-pruned.csv
-grep -v gc 4926314-no-streamdecoder-jmh-result.csv > 4926314-no-streamdecoder-jmh-result-pruned.csv
-grep -v gc 4926314-off-on-heap-jmh-result.csv      > 4926314-off-on-heap-jmh-result-pruned.csv
 
