b5d18d1559aa472f40d21e284d2954d68db836a8
Philippe Marschall <philippe.marschall@gmail.com>
2021-02-14 13:01:37 +0100

    remove old branches

--- run.sh
+++ run.sh
@@ -6,9 +6,3 @@ $JAVA_HOME/bin/java -cp target/benchmarks.jar -Djmh.blackhole.mode=COMPILER com.
 export JAVA_HOME=${HOME}/tmp/JDK-4926314/patched/jdk
 $JAVA_HOME/bin/java -cp target/benchmarks.jar -Djmh.blackhole.mode=COMPILER com.github.marschall.readerbenchmarks.Main 4926314-after
 
-export JAVA_HOME=${HOME}/tmp/JDK-4926314/off-on-heap/jdk
-$JAVA_HOME/bin/java -cp target/benchmarks.jar -Djmh.blackhole.mode=COMPILER com.github.marschall.readerbenchmarks.Main 4926314-off-on-heap
-
-export JAVA_HOME=${HOME}/tmp/JDK-4926314/no-streamdecoder/jdk
-$JAVA_HOME/bin/java -cp target/benchmarks.jar -Djmh.blackhole.mode=COMPILER com.github.marschall.readerbenchmarks.Main 4926314-no-streamdecoder
-
